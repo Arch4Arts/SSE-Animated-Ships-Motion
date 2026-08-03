c447df30b1ecd080196605ed0a50d5a285a24506
Arch4Arts <Arch4Arts@gmail.com>
2026-08-03 13:29:58 +0300

    feat: define mass-responsive hull profiles

--- tests/test_curves.py
+++ tests/test_curves.py
@@ -14,12 +14,12 @@ from ship_motion.profiles import MotionProfile, classify_mesh
 class ProfileSelectionTests(unittest.TestCase):
     def test_selects_exact_motion_profiles(self):
         cases = [
-            ("Distant", "shiprowboat01.nif", (6.0, 1.53333335, 7.0, 4.0, 2.0, 4.0, 8.0, 1.0)),
-            ("NarrowPath", "shiprowboat01.nif", (4.5, 1.1, 5.2, 4.0, 2.0, 4.0, 8.0, 1.0)),
-            ("Distant", "shiplongboat01.nif", (15.0, 1.62, 4.6666667, 10.0, 5.0, 15.0, 30.0, 1.25)),
-            ("NarrowPath", "shiplongboat01.nif", (11.25, 1.17, 3.3333333, 10.0, 5.0, 15.0, 30.0, 1.25)),
-            ("Distant", "shiplarge01.nif", (21.3333333, 1.3, 4.5, 10.0, 5.0, 30.0, 30.0, 2.0)),
-            ("NarrowPath", "shiplarge01.nif", (14.6666667, 0.95, 3.3, 10.0, 5.0, 30.0, 30.0, 2.0)),
+            ("Distant", "shiprowboat01.nif", (6.0, 1.53333335, 7.0, 3.0, 1.5, 4.0, 12.0, 1.0)),
+            ("NarrowPath", "shiprowboat01.nif", (4.5, 1.1, 5.2, 3.0, 1.5, 4.0, 12.0, 1.0)),
+            ("Distant", "shiplongboat01.nif", (15.0, 1.62, 4.6666667, 7.5, 3.75, 12.0, 60.0, 1.25)),
+            ("NarrowPath", "shiplongboat01.nif", (11.25, 1.17, 3.3333333, 7.5, 3.75, 12.0, 60.0, 1.25)),
+            ("Distant", "shiplarge01.nif", (21.3333333, 1.3, 4.5, 16.0, 8.0, 32.0, 96.0, 2.0)),
+            ("NarrowPath", "shiplarge01.nif", (14.6666667, 0.95, 3.3, 16.0, 8.0, 32.0, 96.0, 2.0)),
         ]
         for folder, filename, expected in cases:
             with self.subTest(folder=folder, filename=filename):
@@ -29,7 +29,7 @@ class ProfileSelectionTests(unittest.TestCase):
         profile = classify_mesh("NarrowPath", "shiplongboat01Wreck01.nif")
         self.assertEqual(
             dataclasses.astuple(profile)[:8],
-            (11.25, 1.215, 3.5000000250000003, 10.0, 5.0, 15.0, 30.0, 1.25),
+            (11.25, 1.215, 3.5000000250000003, 7.5, 3.75, 12.0, 60.0, 1.25),
         )
 
     def test_rejects_unknown_folder(self):
@@ -49,18 +49,19 @@ class ProfileSelectionTests(unittest.TestCase):
                 self.assertEqual(profile.turn_smoothing_radius, radius)
                 self.assertEqual(profile.sink_offset_units, sink)
 
-    def test_v7_inertial_profiles_match_hull_mass(self):
+    def test_v8_profiles_encode_mass_response(self):
         cases = [
-            ("rowboat", "shiprowboat01.nif", (2.0, 3, 0.75, 7.0, -19.0)),
-            ("longboat", "shiplongboat01.nif", (6.0, 6, 0.65, 4.6666667, -24.5)),
-            ("large", "shiplarge01.nif", (15.0, 10, 0.55, 4.5, 0.0)),
+            ("shiprowboat01.nif", (3.0, 4.0, 12.0, 45.0, 0.20, 1.5, 0.16, 0.14)),
+            ("shiplongboat01.nif", (7.5, 12.0, 60.0, 55.0, 0.75, 3.75, 0.13, 0.11)),
+            ("shiplarge01.nif", (16.0, 32.0, 96.0, 65.0, 2.50, 8.0, 0.08, 0.06)),
         ]
-        for _, filename, expected in cases:
+        for filename, expected in cases:
             with self.subTest(filename=filename):
                 profile = classify_mesh("Distant", filename)
-                self.assertEqual((profile.yaw_sigma_seconds, profile.turn_lookahead_segments,
-                                  profile.minimum_turn_speed, profile.roll_degrees,
-                                  profile.sink_offset_units), expected)
+                self.assertEqual((profile.heave_pitch_period_seconds, profile.roll_period_seconds,
+                                  profile.loop_duration_seconds, profile.pitch_phase_degrees,
+                                  profile.pitch_smoothing_sigma_seconds, profile.harmonic_period_seconds,
+                                  profile.heave_harmonic_weight, profile.pitch_harmonic_weight), expected)
         narrow_long = classify_mesh("NarrowPath", "shiplongboat01.nif")
         self.assertAlmostEqual(narrow_long.roll_degrees, 3.3333333)
         self.assertEqual(narrow_long.sink_offset_units, -24.5)
@@ -74,6 +75,24 @@ class ProfileSelectionTests(unittest.TestCase):
             (normal.yaw_sigma_seconds, normal.turn_lookahead_segments,
              normal.minimum_turn_speed, normal.sink_offset_units),
         )
+        self.assertEqual(
+            (wreck.heave_pitch_period_seconds, wreck.harmonic_period_seconds,
+             wreck.roll_period_seconds, wreck.loop_duration_seconds,
+             wreck.pitch_phase_degrees, wreck.pitch_smoothing_sigma_seconds,
+             wreck.heave_harmonic_weight, wreck.pitch_harmonic_weight),
+            (normal.heave_pitch_period_seconds, normal.harmonic_period_seconds,
+             normal.roll_period_seconds, normal.loop_duration_seconds,
+             normal.pitch_phase_degrees, normal.pitch_smoothing_sigma_seconds,
+             normal.heave_harmonic_weight, normal.pitch_harmonic_weight),
+        )
+
+    def test_folder_variants_share_timing_but_not_amplitude(self):
+        for filename in ("shiprowboat01.nif", "shiplongboat01.nif", "shiplarge01.nif"):
+            distant = classify_mesh("Distant", filename)
+            narrow = classify_mesh("NarrowPath", filename)
+            self.assertNotEqual((distant.heave_units, distant.pitch_degrees),
+                                (narrow.heave_units, narrow.pitch_degrees))
+            self.assertEqual(dataclasses.astuple(distant)[3:8], dataclasses.astuple(narrow)[3:8])
 
 
 class MotionCurveTests(unittest.TestCase):
